d62bb497754fe1d1fe3d6a0504c181cadfadfae4
Jimaxo2 <jimaxogamer@gmail.com>
2025-11-27 08:27:58 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPT_Juego_Servidor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Espacio para trabajar el Servidor
+        }
+    }
+}
